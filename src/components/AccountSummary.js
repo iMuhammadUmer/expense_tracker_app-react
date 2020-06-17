f17ec11389f6b0abff7013ed2a93cfbdc04a2597
Muhammad Umer <mumer1297@gmail.com>
2020-06-17 06:56:27 +0500

    [update] AccountSummary

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -5,9 +5,11 @@ export const AccountSummary = () => {
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
+        <p className="money plus">0.00</p>
       </div>
       <div>
         <h4>Expense</h4>
+        <p className="money plus">0.00</p>
       </div>
     </div>
   );
