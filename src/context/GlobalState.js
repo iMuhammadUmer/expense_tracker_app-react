f760d491fadfd51eb7b3fc1d2bb515250b98665f
Muhammad Umer <mumer1297@gmail.com>
2020-06-18 10:39:04 +0500

    update initialTransactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,28 +1,11 @@
-import React, { createContext, useReducer } from "react";
+import { createContext } from "react";
 
-import AppReducer from "./AppReducer";
-
-const initialState = {
+const initialTransactions = {
   transactions: [
-    { id: 1, description: "Income 1", transactionAmount: 1000 },
-    { id: 2, description: "Expense 1", transactionAmount: -100 },
-    { id: 3, description: "Income 2", transactionAmount: 2000 },
-    { id: 4, description: "Expense 2", transactionAmount: -500 },
+    { amount: 100, desc: "Cash" },
+    { amount: -40, desc: "Book" },
+    { amount: -200, desc: "Camera" },
   ],
 };
 
-export const GlobalContext = createContext(initialState);
-
-export const GlobalProvider = ({ children }) => {
-  const [state] = useReducer(AppReducer, initialState);
-
-  return (
-    <GlobalContext.Provider
-      value={{
-        transactions: state.transactions,
-      }}
-    >
-      {children}
-    </GlobalContext.Provider>
-  );
-};
+export const TransactionContext = createContext(initialTransactions);
