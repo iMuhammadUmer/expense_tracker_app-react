66fc20bd9b33fe3896b1b21365eb3812e1343855
Muhammad Umer <mumer1297@gmail.com>
2020-06-20 06:42:43 +0500

    update imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 
-// Import the Global Provider
 import { GlobalProvider } from "./context/GlobalState";
 
-// Import Components
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { AccountSummary } from "./components/AccountSummary";
