3fca57599cca58e61ac78d8a7524916fb0a566d2
Muhammad Umer <mumer1297@gmail.com>
2020-06-20 06:19:26 +0500

    update imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header.js";
-import { Balance } from "./components/Balance.js";
+
+// Import the Global Provider
+import { GlobalProvider } from "./context/GlobalState";
+
+// Import Components
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 import { AccountSummary } from "./components/AccountSummary";
 import { TransactionHistory } from "./components/TransactionHistory";
 import { AddTransaction } from "./components/AddTransaction";
-import { initialTransactions } from "./context/GlobalState";
 
 function App() {
   return (
-    <initialTransactions>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -17,7 +21,7 @@ function App() {
         <TransactionHistory />
         <AddTransaction />
       </div>
-    </initialTransactions>
+    </GlobalProvider>
   );
 }
 
