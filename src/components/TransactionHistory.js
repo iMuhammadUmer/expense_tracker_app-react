6f577b9701acbb169bf72b77264ba490a9cf235c
Muhammad Umer <mumer1297@gmail.com>
2020-06-21 11:37:29 +0500

    update functionality

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 
-import { GlobalContext } from "../context/GlobalState";
-
+// Import Transaction Component
 import { Transaction } from "./Transaction";
+
+// Import the Global State
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionHistory = () => {
   const { transactions } = useContext(GlobalContext);
