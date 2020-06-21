3e08613879370e27508002042a476ccc256c4a9c
Muhammad Umer <mumer1297@gmail.com>
2020-06-21 11:37:11 +0500

    update functionality

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// Import the Global State
+import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const transactionAmounts = transactions.map(
+    (transaction) => transaction.transactionAmount
+  );
+  const balance = transactionAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
   return (
     <div>
-      <h2>Current Balance</h2>
-      <h1>0.00 PKR</h1>
+      <h4>Current Balance</h4>
+      <h1 id="balance">${balance}</h1>
     </div>
   );
 };
